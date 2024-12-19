472f5baf1b331670d0ff849cd3809312fc633095
Rubick <eithanllanosb@gmail.com>
2024-12-19 09:45:38 -0500

    set up spring initializr base implementation

---
+++ MVCsecuritydemo/src/main/java/com/springCourse/MVCsecuritydemo/MvCsecuritydemoApplication.java
@@ -0,0 +1,13 @@
+package com.springCourse.MVCsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvCsecuritydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvCsecuritydemoApplication.class, args);
+	}
+
+}
