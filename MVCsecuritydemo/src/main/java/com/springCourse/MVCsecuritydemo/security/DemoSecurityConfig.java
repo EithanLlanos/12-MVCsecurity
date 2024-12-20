3f7b9968518a7e16670da66f7c688bf0bde9f089
Rubick <eithanllanosb@gmail.com>
2024-12-20 14:31:31 -0500

    Add logout functionality and html implementation

--- MVCsecuritydemo/src/main/java/com/springCourse/MVCsecuritydemo/security/DemoSecurityConfig.java
+++ MVCsecuritydemo/src/main/java/com/springCourse/MVCsecuritydemo/security/DemoSecurityConfig.java
@@ -32,7 +32,10 @@ public class DemoSecurityConfig {
                                 .loginPage("/showMyLoginPage")
                                 .loginProcessingUrl("/authenticateTheUser")
                                 .permitAll()
-                );
+                )
+                .logout(logout -> logout.permitAll())
+        ;
+
         return http.build();
     }
 }
